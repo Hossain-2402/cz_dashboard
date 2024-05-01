18c9fff610a9673f6a65c594aa09f467537c278a
Hossain-Ibna-Ehsan <hossain.ie2402@gmail.com>
2024-05-01 21:59:10 +0600

    cz_web_app

--- src/Cart.js
+++ src/Cart.js
@@ -9,17 +9,26 @@ function Cart() {
 
   const [positionOfDetailArea,setPositionOfDetailArea] = useState("-200vw");
   const [tempLeadingImage,setTempLeadingImage] = useState("https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/438216908_990245046437412_764419811823206966_n.jpg?stp=cp6_dst-jpg&_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_ohc=4kFdMbs7jXwAb6jmRp9&_nc_ht=scontent.fdac41-1.fna&oh=00_AfBc9w3xDgUjGjP9zpoY-cri1zMj7RcFkrXVFmzdB7R3OQ&oe=6636DC71");
+  const [tempIndex,setTempIndex] = useState();  
+  const [tempQuantity,setTempQuantity] = useState(1);
 
 
 
-  const showDetailArea = (item)=>{
+  const showDetailArea = (item,index)=>{
     setPositionOfDetailArea("0vw");
     setTempLeadingImage(item.leading_image);
+    setTempIndex(index);
+    setTempQuantity(item.quantity);
   }
   const hideDetailArea = ()=>{
     setPositionOfDetailArea("-200vw");
   }
-
+  const increaseQuantity = ()=>{
+    let a = tempQuantity;
+    setTempQuantity(a+1);
+    items[tempIndex].quantity = items[tempIndex].quantity + 1;
+    console.log(items[tempIndex].quantity);
+  }
 
   return (
     <div className="Cart">
@@ -56,13 +65,18 @@ function Cart() {
               furthering a social cause. Businesses range in scale and scope from sole proprietorships to large, international corporations.
           </div>
           <input type="text" className="sizes" placeholder="Enter your measurements"/>          
+          <div className="quantity_area">
+            <div className="decrease_auantity_btn">-</div>
+            <div className="quantity">{tempQuantity}</div>
+            <div className="increase_auantity_btn" onClick={()=>{increaseQuantity()}}>+</div>
+          </div>
           {/*<div className="add_to_cart_btn" onClick={()=>{addItemToCart()}}>Add to cart</div>*/}
         </div>
       </div>
       {items.length === 0 ? <div className="no_item_display_area">Cart is empty</div> :
         <div className="products_area">
           {items.map((item,index) =>{
-            return <div key={index} className="product" onClick={()=>{showDetailArea(item)}}>
+            return <div key={index} className="product" onClick={()=>{showDetailArea(item,index)}}>
               <div style={{ backgroundImage: "url("+item.leading_image+")",backgroundRepeat : 'no-repeat',backgroundSize : '85%'}}  className="image"></div>
               <div className="product_name">Product Name</div>
               <div className="price">$55</div>
