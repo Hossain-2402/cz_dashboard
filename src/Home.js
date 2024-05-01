e02fbc6fd995bc930bd749b9e769c1b801597eee
Hossain-Ibna-Ehsan <hossain.ie2402@gmail.com>
2024-05-01 17:36:24 +0600

    cz

--- src/Home.js
+++ src/Home.js
@@ -2,55 +2,80 @@ import './Home.css';
 import {useState,useEffect} from "react";
 import db from "./firebase";
 import firebase from "firebase/compat/app";
+import {useSelector,useDispatch} from "react-redux";
+import {add_to_cart_action} from "./redux_file"
 
 function Home() {
 
 
   const [products,setProducts] = useState([]);
   const [positionOfDetailArea,setPositionOfDetailArea] = useState("-200vw");
+  const [tempNewItemForCart,setTempNewItemForCart] = useState();
+  const items = useSelector(state => state.products);
+  const dispatch = useDispatch();
 
-  const img = "https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/438216908_990245046437412_764419811823206966_n.jpg?stp=cp6_dst-jpg&_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_ohc=4kFdMbs7jXwAb6jmRp9&_nc_ht=scontent.fdac41-1.fna&oh=00_AfBc9w3xDgUjGjP9zpoY-cri1zMj7RcFkrXVFmzdB7R3OQ&oe=6636DC71";
+  const [tempLeadingImage,setLeadingImage] = useState("https://scontent.fdac41-1.fna.fbcdn.net/v/t39.30808-6/438216908_990245046437412_764419811823206966_n.jpg?stp=cp6_dst-jpg&_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_ohc=4kFdMbs7jXwAb6jmRp9&_nc_ht=scontent.fdac41-1.fna&oh=00_AfBc9w3xDgUjGjP9zpoY-cri1zMj7RcFkrXVFmzdB7R3OQ&oe=6636DC71");
 
   useEffect(()=>{
-    db.collection('products').onSnapshot(snapshot=>{
+    db.collection('products').orderBy("timestamp","asc").onSnapshot(snapshot=>{
       setProducts(snapshot.docs.map(doc => doc.data()));
     });
   },[]);
 
   const addItem = ()=>{
-    db.collection('products').add(
-      {
-        product_name : "Product Name",
-        product_detail : " Product detail" ,
-        leading_image : "leading Image",
-        first_image : "first image",
-        second_image : "second image",
-        third_image : "third image",
-        forth_image : "forth image",
-        sizes : "1",
-        timestamp : firebase.firestore.FieldValue.serverTimestamp()
-    }) ;
+    // db.collection('products').add(
+    //   {
+    //     product_name : "Product Name",
+    //     product_detail : " Product detail" ,
+    //     leading_image : "leading Image",
+    //     first_image : "first image",
+    //     second_image : "second image",
+    //     third_image : "third image",
+    //     forth_image : "forth image",
+    //     sizes : "1",
+    //     timestamp : firebase.firestore.FieldValue.serverTimestamp()
+    // }) ;
+    console.log(items.length);
   }
 
-  const showDetailArea = ()=>{
+  const showDetailArea = (item)=>{
     setPositionOfDetailArea("0vw");
+    setLeadingImage(item.leading_image);
+    setTempNewItemForCart(item);
   }
   const hideDetailArea = ()=>{
     setPositionOfDetailArea("-200vw");
   }
+  const addItemToCart = ()=>{
+  
+  let alreadyAdded = false;
+  for(let i=0;i<items.length;i++){
+    if(items[i].timestamp === tempNewItemForCart.timestamp){
+      alreadyAdded = true;
+      break;
+    }
+    else{
+      continue;
+    }
+  }
+  if(!alreadyAdded) {
+    dispatch(add_to_cart_action(tempNewItemForCart))
+  }
+
+}
 
 
 
 
   return (
     <div className="Home">
-    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
+      <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
       <link rel="styleSheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 
       <div style={{ left: positionOfDetailArea}} className="detail_of_product_area">
         <div className="close_detail_Area_btn" onClick={()=>{hideDetailArea()}}><i class="fa fa-times" ></i> </div>
         <div  className="detail_image_area">
-          <div style={{ backgroundImage: "url("+img+")",backgroundRepeat : 'no-repeat',backgroundSize : '50%',backgroundPosition : 'center center'}} className="leading_image"></div>
+          <div style={{ backgroundImage: "url("+tempLeadingImage+")",backgroundRepeat : 'no-repeat',backgroundSize : '50%',backgroundPosition : 'center center'}} className="leading_image"></div>
           <div className="secondary_image">
             <div className="first_image"></div>
             <div className="second_image"></div>
@@ -71,22 +96,28 @@ function Home() {
               industrial, or professional activities. The purpose of a business is to organize some sort of economic production 
               of goods or services. Businesses can be for-profit entities or non-profit organizations fulfilling a charitable mission or
               furthering a social cause. Businesses range in scale and scope from sole proprietorships to large, international corporations.
+
+
+              furthering a social cause. Businesses range in scale and scope from sole proprietorships to large, international corporations.The term business refers to an organization or enterprising entity engaged in commercial, 
+              industrial, or professional activities. The purpose of a business is to organize some sort of economic production 
+              of goods or services. Businesses can be for-profit entities or non-profit organizations fulfilling a charitable mission or
+              furthering a social cause. Businesses range in scale and scope from sole proprietorships to large, international corporations.
           </div>
           <input type="text" className="sizes" placeholder="Enter your measurements"/>          
-          <div className="add_to_cart_btn">Add to cart</div>
+          <div className="add_to_cart_btn" onClick={()=>{addItemToCart()}}>Add to cart</div>
         </div>
       </div>
 
       <div className="header">
         <div className="header_text">Medium length section heading goes here  </div>
         <div className="sub_header_text">The term business refers to an organization or enterprising entity engaged in commercial, industrial, or professional activities. The purpose of a business is to organize some sort of economic production of goods or services. Businesses can be for-profit entities or non-profit organizations fulfilling a charitable mission or furthering a social cause. Businesses range in scale and scope from sole proprietorships to large, international corporations. </div>
-        <div className="cart_btn">Cart</div>
+        <div className="cart_btn" onClick={()=>{addItem()}}>Cart</div>
       </div>
 
       <div className="products_area">
         {products.map((item,index) =>{
-          return <div key={index} className="product" onClick={()=>{showDetailArea()}}>
-            <div className="image"></div>
+          return <div key={index} className="product" onClick={()=>{showDetailArea(item)}}>
+            <div style={{ backgroundImage: "url("+item.leading_image+")",backgroundRepeat : 'no-repeat',backgroundSize : '85%'}}  className="image"></div>
             <div className="product_name">Product Name</div>
             <div className="price">$55</div>
           </div>
@@ -95,6 +126,7 @@ function Home() {
 
       <div className="footer">
           <div className="footer_header">
+            <div className="gap"></div>
             <div className="logo_a">Logo </div>
             <div className="nev"></div>
             <div className="socials">
@@ -103,7 +135,6 @@ function Home() {
               <a href="https://www.google.com/" className="twitter_btn">Twitter</a>
               <a href="https://www.google.com/" className="thread_btn">Thread</a>
             </div>
-            <div className="gap"></div>
           </div>
             <div className="line"></div>
             <div className="copyright">© 2024 CZ. All rights reserved.</div>
